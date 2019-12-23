adf32ef8f05b2e46647622ed4db3f1f475b867b6
RosieLeaman <rosie.leaman@gmail.com>
2019-12-23 17:11:11 +0000

    initial commit

---
+++ drawing.js
@@ -0,0 +1,190 @@
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+function nextPoint(posX,posY,prevDirectionX,prevDirectionY,xMax,yMax){
+  // if currentDirection is fine then move along
+  if (posX + prevDirectionX < xMax & posX + prevDirectionX >= 0 & posY + prevDirectionY < yMax & posY + prevDirectionY >= 0){
+    return [posX + prevDirectionX,posY + prevDirectionY,prevDirectionX,prevDirectionY]
+  }
+  if (posX + prevDirectionX >= xMax | posX + prevDirectionX < 0){
+    return [posX - prevDirectionX,posY + prevDirectionY,-prevDirectionX,prevDirectionY]
+  }
+  if (posY + prevDirectionY >= yMax | posY + prevDirectionY < 0){
+    return [posX + prevDirectionX,posY - prevDirectionY,prevDirectionX,-prevDirectionY]
+  }
+  return null
+}
+
+function makePathFrom(initialPointX,initialPointY,xMax,yMax,startFlip){
+  var posX = initialPointX;
+  var posY = initialPointY;
+  var prevDirectionX = 1;
+  var prevDirectionY = 1;
+
+  // var lineData = [{"x":posX,"y":posY}];
+  var lineData = [];
+
+  // decided to pass startFlip in case don't want to start with an over weave
+  // but with the current approach of going along top line not sure it is necessary
+  // as all top lines go over?
+  var flip = startFlip;
+
+  var finished = false
+
+  while (finished == false){
+
+    var next = nextPoint(posX,posY,prevDirectionX,prevDirectionY,xMax,yMax);
+
+    posX = next[0],posY = next[1], prevDirectionX = next[2], prevDirectionY = next[3];
+
+    if (posX > 0 & posX < xMax-1 & posY > 0 & posY < yMax-1){
+      if (flip == 1){
+        lineData.push({"x":posX-0.2*prevDirectionX,"y":posY-0.2*prevDirectionY})
+        lineData.push({"x":null,"y":null})
+        lineData.push({"x":posX+0.2*prevDirectionX,"y":posY+0.2*prevDirectionY})
+      }
+      else{
+        lineData.push({"x":posX,"y":posY})
+      }
+      flip = -flip
+    }
+    else{
+      lineData.push({"x":posX,"y":posY})
+    }
+
+    if (posX == initialPointX & posY == initialPointY){
+      finished = true
+    }
+
+  }
+
+  // have to repeat one more step once complete to get the loop to complete
+  var next = nextPoint(posX,posY,prevDirectionX,prevDirectionY,xMax,yMax);
+
+  posX = next[0],posY = next[1], prevDirectionX = next[2], prevDirectionY = next[3];
+
+  if (posX > 0 & posX < xMax-1 & posY > 0 & posY < yMax-1){
+    if (flip == 1){
+      lineData.push({"x":posX-0.2*prevDirectionX,"y":posY-0.2*prevDirectionY})
+      lineData.push({"x":null,"y":null})
+      lineData.push({"x":posX+0.2*prevDirectionX,"y":posY+0.2*prevDirectionY})
+    }
+    else{
+      lineData.push({"x":posX,"y":posY})
+    }
+    flip = -flip
+  }
+  else{
+    lineData.push({"x":posX,"y":posY})
+  }
+
+  return lineData
+}
+
+function findNextEdgePoint(lines,xMax){
+  // investigate the top line, if all points have been visited (if region has no holes) then we are done
+  for (var x=1;x<2*xMax-1;x=x+2){
+    var found = false
+    for (var i=0;i<lines.length;i++){
+      // check each line
+      for (var j=0;j<lines[i].length;j++){
+        // check each point
+        if (lines[i][j].y == 0 & lines[i][j].x == x){
+          found = true
+          break
+        }
+      }
+    }
+    if (found == false){
+      return x
+    }
+  }
+  return null
+}
+
+function makePath(xMax,yMax){
+  var lines = []
+
+  var initialPointY = 0
+
+  initialPointX = findNextEdgePoint(lines,xMax)
+
+  while (initialPointX !== null){
+    lines.push(makePathFrom(initialPointX,initialPointY,2*xMax-1,2*yMax-1,1))
+    initialPointX = findNextEdgePoint(lines,xMax)
+  }
+
+
+  return lines
+}
+
+function drawPath(lineData,gapX,gapY,colour){
+  // convert to drawn co-ordinates
+  lineDataCopy = []
+  for (var i=0;i<lineData.length;i++){
+    if (lineData[i].x !== null){
+      lineDataCopy.push({'x':lineData[i].x*gapX,'y':lineData[i].y*gapY})
+    }
+    else{
+      lineDataCopy.push({'x':null,'y':null})
+    }
+  }
+
+  // draw it
+  var lineFunction = d3.line()
+                       .x(function(d) { return d.x; })
+                       .y(function(d) { return d.y; })
+                       .defined(function (d) { return !(d.x == null); })
+                       .curve(d3.curveBasis)
+
+  var lineGraph = svgContainer.append("path")
+                              .attr("d", lineFunction(lineDataCopy))
+                              .attr("stroke", colour)
+                              .attr("stroke-width", 2)
+                              .attr("fill", "none");
+}
+
+function drawAllPaths(lines,gapX,gapY){
+  colourCycle = ["blue","red","black"]
+  for (var i=0;i<lines.length;i++){
+    drawPath(lines[i],gapX,gapY,colourCycle[i])
+  }
+}
+
+function drawCircles(sizeX,sizeY,gapX,gapY){
+  var circleRadii = []
+  for (var i=0;i<sizeX*sizeY;i++){
+    circleRadii.push(5)
+  }
+
+  circles = svgContainer.selectAll("circle")
+                        .data(circleRadii)
+
+  circles.enter()
+         .append("circle")
+         .attr("cx",function(d,i){
+           return gapX*(i%sizeX);
+         })
+         .attr("cy",function(d,i){
+           return gapY*(Math.floor(i/sizeX)%sizeY);
+         })
+         .attr("r",function(d){
+           return d;
+         })
+}
+
+var sizeX = 3
+var sizeY = 20
+var gapX = 50
+var gapY = 50
+
+svgContainer = d3.select("body")
+                 .append("svg")
+                 .attr("width",(sizeX-1)*gapX)
+                 .attr("height",(sizeY-1)*gapY)
+
+// make the line path
+var linePaths = makePath(sizeX,sizeY)
+
+drawAllPaths(linePaths,gapX/2,gapY/2)
